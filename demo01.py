18e60787237eaf75829fab222be0f09afdaeb9f8
KongFanchuan <2975607430@qq.com>
2021-04-12 16:13:12 +0800

    第一次相github提交代码

---
+++ demo01.py
@@ -0,0 +1,12 @@
+# """
+# print('你好')
+# print(1+2)
+# print(100*100)
+# print(1+2+3)  
+# print('哈'*100)
+# print("ha"*100)
+# """
+a = input("请输入：")
+b = input("请输入：")
+print("获取的内容:",a+b)
+print("字符长度是:",len(a+b))
